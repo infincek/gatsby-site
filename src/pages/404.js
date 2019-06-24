8ea1e12b6a15a5e5fc4eea0e35d922f6130687a6
Athul-CA <athul8720@gmail.com>
2019-06-24 15:06:10 +0530

    Added a 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Header from "../components/header"
+
+class NotFoundPage extends React.Component{
+        render(){
+                const {data}=this.props
+                const siteTitle=data.site.siteMetaData.title
+
+                return (
+                        <Header location={this.props.location} title={siteTitle}>
+                        <h1>Not Found</h1>
+                        <p>You just hit a route that <b>Doesn&#39;t Exist</b>.... We recommend you to check out the other things.....</p>
+                        </Header>
+                )
+        }
+}
+export default NotFoundPage
+
+export const pagequery= graphql`
+query{
+  site{
+    siteMetadata{
+        title
+        }
+      }
+}
+`
