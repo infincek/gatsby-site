80ad4d2ee36002d6badd14148efb72174ea4c31f
Samanyu Neelson <samanyuneel@gmail.com>
2019-09-27 13:37:03 +0530

    tender and news pages

---
+++ src/components/tenders.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import marked from 'marked';
+
+export default class props extends React.Component {
+    render() {
+        return (
+            <StaticQuery
+                query={graphql`
+                    query {
+                        allTendersYaml(limit: 5) {
+                            edges {
+                                node {
+                                    data
+                                    new
+                                    date
+                                }
+                            }
+                        }
+                    }
+                `}
+                render={data => (
+                    <React.Fragment>{getData(data)}</React.Fragment>
+                )}
+            />
+        );
+    }
+}
+
+function getData(data) {
+    const ann = [];
+    data.allTendersYaml.edges.forEach(function(item, i) {
+      console.log(item)
+        ann.push(
+            <ListItem key={"tender"+i} data={item.node}/>
+        );
+    });
+    return ann;
+}
+
+const ListItem = ({data}) => {
+    return(
+        <li className="collection-item">
+            {data.new &&
+                <span className="badge">new</span>
+            }
+            <div dangerouslySetInnerHTML={{ __html: marked(data.data) }}></div>
+            {
+                data.date &&
+                <span className="date"><i className="fa fa-calendar"></i> {data.date}</span>
+            }
+        </li>
+    )
+}
