ef77e4c77ec1c3dc35c3daad701e5790977d0f33
Samanyu Neelson <samanyuneel@gmail.com>
2019-10-10 20:23:26 +0530

    faculty changes

---
+++ src/pages/faculty.js
@@ -0,0 +1,137 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from '../components/layout';
+import Faculties from "../components/faculties";
+
+
+const meta = {
+    title: "Faculty",
+    keywords: "Faculty, hod, teaching, non teachings",
+    description: "All faculty of College of Engineering Kidangoor"
+};
+
+export default ({data}) => {
+    return (
+        <Layout meta={meta}>
+            <div className="page-contents container">
+                <div className="title center">
+                    <h1 className="underlined">
+                        <span>Faculty</span>
+                    </h1>
+                </div>
+                <div className="contents">
+                    <div className="row">
+                        <h2>HODs</h2>
+                        <Faculties data={data.hod}/>
+                    </div>
+                    <div className="row">
+                        <h2>Teaching</h2>
+                        <Faculties data={data.faculties}/>
+                    </div>
+                    <div className="row">
+                        <h2>Technical Staff</h2>
+                        <Faculties data={data.technical}/>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query{
+        faculties: allMarkdownRemark(
+            filter: {
+                frontmatter: {  hod: {eq: false} teaching: { ne: false} }
+                fileAbsolutePath: { regex: "/faculty/" }
+            },
+            sort: {fields: frontmatter___position}
+        ) {
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                        name
+                        hod
+                        departmentFullName
+                        image {
+                            publicURL
+                            childImageSharp {
+                                fluid(maxWidth: 1000) {
+                                    srcSet
+                                    ...GatsbyImageSharpFluid_tracedSVG
+                                }
+                            }
+                        }
+                        designation
+                    }
+                    fields {
+                        slug
+                    }
+                }
+            }
+        }
+        technical: allMarkdownRemark(
+            filter: {
+                frontmatter: { teaching: {eq: false} }
+                fileAbsolutePath: { regex: "/faculty/" }
+            }
+        ) {
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                        name
+                        departmentFullName
+                        image {
+                            publicURL
+                            childImageSharp {
+                                fluid(maxWidth: 1000) {
+                                    srcSet
+                                    ...GatsbyImageSharpFluid_tracedSVG
+                                }
+                            }
+                        }
+                        designation
+                    }
+                    fields {
+                        slug
+                    }
+                }
+            }
+        }
+        hod: allMarkdownRemark(
+            filter: {
+                frontmatter: { hod: {eq: true} }
+                fileAbsolutePath: { regex: "/faculty/" }
+            }
+        ) {
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                        name
+                        hod
+                        departmentFullName
+                        image {
+                            publicURL
+                            childImageSharp {
+                                fluid(maxWidth: 1000) {
+                                    srcSet
+                                    ...GatsbyImageSharpFluid_tracedSVG
+                                }
+                            }
+                        }
+                        designation
+                    }
+                    fields {
+                        slug
+                    }
+                }
+            }
+        }
+    }
+` 
